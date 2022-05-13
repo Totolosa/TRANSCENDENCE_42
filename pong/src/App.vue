f406b3d747230cf353dad1ab8006f2e4b1786421
Thomas Dayde <tdayde@z3r7p3.42lyon.fr>
2022-05-13 19:39:29 +0200

    first commit game

---
+++ pong/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <HelloWorld msg="COUCOU TWA"/> -->
+  <GameField/>
+  
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+// import HelloWorld from './components/HelloWorld.vue';
+import GameField from './components/GameField.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    // HelloWorld,
+    GameField
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ pong/src/components/GameField.vue
@@ -0,0 +1,57 @@
+<template>
+	<div>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import * as THREE from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+
+let scene: THREE.Scene;
+
+export default defineComponent({
+	name: 'GameField',
+	props: {
+		containerId: {
+			type: String,
+			required: true
+		},
+		modelSettings: {
+			type: Object,
+			required: true
+		}
+	},
+	data() {
+		return {
+			scene: new THREE.Scene,
+			// camera: undefined,
+			// renderer: undefined
+		}
+	},
+	methods: {
+		// renderScene() {
+		// 	// if (this.renderer)
+		// 		(this.renderer!).render(this.scene, this.camera);
+		// },
+		init() {
+			// if (typeof this.scene === "Scene")
+				this.scene = new THREE.Scene();
+			// this.scene.backgroud = null;
+			// this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+			// this.renderer.outputEncoding = THREE.sRGBEncoding;
+			// this.renderer.setPixelRatio(window.devicePixelRatio);
+		}
+	},
+	mounted () {
+		this.init();
+	}
+})
+</script>
+
+<style scoped>
+
+</style>
+
